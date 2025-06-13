29f4c1a136a14efb4f2e932ba517019d5eb88c17
Wayne <kleavonwayne6270@gmail.com>
2025-06-13 07:50:56 +0200

    backend stuff

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,151 @@
+import User from "../models/userModel.js";
+import bcrypt, { hash } from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
+
+export const signupUser = async (req, res) => {
+  try {
+    const { name, email, username, password } = req.body;
+
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User with that email or username already exists." });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    generateTokenAndSetCookie(newUser._id, res);
+
+    res.status(201).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      username: newUser.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in signupUser controller: ", error.message);
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password || "");
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in loginUser controller", error.message);
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  try {
+    res.cookie("threads_jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in logoutUser controller", error.message);
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString())
+      return res.status(400).json({ message: "You cannot follow/unfollow yourself" });
+
+    if (!userToModify || !currentUser)
+      return res.status(404).json({ message: "User to follow/unfollow not found." });
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      //Unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in followUnfollowUser controller", error.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    if (req.params.id !== userId.toString())
+      return res.status(400).json({ message: "You cannot update other users's profile" });
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+
+    res.status(200).json({ message: "Profile updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in updateUser controller", error.message);
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username })
+      .select("-password")
+      .select("-updatedAt");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+    console.error("Error in getUserProfile controller", error.message);
+  }
+};
