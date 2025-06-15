962751f3bbf13e21958511ed7d365616e7cf8486
Wayne <kleavonwayne6270@gmail.com>
2025-06-15 10:20:27 +0200

    deploy try

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,15 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import {app, server }from "./socket/socket.js"
+import path from "path"
 
 dotenv.config();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -28,5 +31,13 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
